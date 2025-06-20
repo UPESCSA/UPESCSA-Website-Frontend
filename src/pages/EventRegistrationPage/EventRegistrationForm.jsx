8492a2c255b19357f25baa3cdfe523beb38aeffa
Rudra Gupta <88350774+BlackJack-14@users.noreply.github.com>
2025-06-20 23:02:11 +0530

    WebGenesis : Week 1 Registration Closed

--- src/pages/EventRegistrationPage/EventRegistrationForm.jsx
+++ src/pages/EventRegistrationPage/EventRegistrationForm.jsx
@@ -214,6 +214,7 @@ const EventRegistrationForm = () => {
     "Week 3: DevOps & Automation",
     "Week 4: Cloud Deployment",
   ];
+  const disabledOptions = ["Week 1: Web Development"]; // Options that should be disabled in the dropdown
 
   // VALIDATION FUNCTIONS
   // const VALIDATESESSION = (value, setValid) => {
@@ -490,7 +491,7 @@ const EventRegistrationForm = () => {
                     inputLabel="Select Sessions"
                     required={true}
                     options={options}
-                    disabledOptions={[""]}
+                    disabledOptions={disabledOptions}
                   />
                   {/* <DropDownSelectField
                       id="participantCSAMember"
